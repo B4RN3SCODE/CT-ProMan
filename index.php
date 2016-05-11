7c4e20a5d843b2f8ce2ec400ae2b8878aaac92c5
Tyler J Barnes <b4rn3scode@gmail.com>
2016-05-11 10:19:32 -0700

    YT

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-phpinfo();
-?>
+<html>
+	<head>
+		<title>Epic shit til I write some code</title>
+	</head>
+	<body>
+		<div>
+			<iframe width="100%" height="100%" src="https://www.youtube.com/embed/mv5pjz0fIjg" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</body>
+</html>
